9976a66960f2d1fefe241350a4e7468bce7b3c4a
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2024-02-18 22:48:09 +0530

    stable sil

--- app/index.php
+++ app/index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,132 +17,118 @@
             <div class="nav-two">
                 <a href="./index.php">Home</a>
                 <a href="./leaderboard.php">Leaderboard</a>
-                <!-- <a href="./view_activities.php">View Activities</a> -->
                 <a href="./add_grievance.php">Grievances</a>
                 <a href="./admin.php">Admin</a>
             </div>
         </div>
     </div>
 
-    <div class="formdiv">
-        <form action="" method="post">
-            <input type="text" id="username" name="username" placeholder="Student University ID" required>
-            <br>
-            <input type="submit" value="Search Participation">
-        </form>
-        <p class="developedby">Designed and Developed by ZeroOne Code Club. All rights reserved</p>
+    <div class="main-content">
+        <div class="formdiv">
+            <form action="" method="post">
+                <input type="text" id="username" name="username" placeholder="Student University ID" required>
+                <input type="submit" value="Search Participation">
+            </form>
+        </div>
+
+        <div class="points-table">
+                <?php
+                    require_once 'dbconn.php';
+                    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    $username = $_POST['username'];
+                    
+                    $sql = "SELECT * FROM participants WHERE username = ?";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param('s', $username);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+
+                    if ($result->num_rows > 0) {
+                        echo "<div class='points_table_div'>";
+                        echo "<h3>Category-wise Points for $username</h3>";
+                        echo "<table class='category_table'>";
+                            echo "<tr>
+                                    <th>Category</th>
+                                    <th>Activities Participated</th>
+                                    <th>Points</th>
+                                    <th>Percentage</th>
+                                </tr>";
+
+                            $categoryPoints = array('TEC' => 0, 'ESO' => 0, 'LCH' => 0, 'IIE' => 0, 'HWB' => 0);
+                            $categoryEvents = array('TEC' => 0, 'ESO' => 0, 'LCH' => 0, 'IIE' => 0, 'HWB' => 0);
+
+                            while ($row = $result->fetch_assoc()) {
+                                $categoryPoints[$row['category']] += $row['points'];
+                                $categoryEvents[$row['category']] += 1;
+                            }
+
+                            foreach ($categoryPoints as $category => $points) {
+                                echo "<tr>
+                                        <td>$category</td>
+                                        <td>{$categoryEvents[$category]}</td>
+                                        <td>$points / 200</td>
+                                        <td>".($points/2)."%</td>
+                                    </tr>";
+                            }
+
+                            $totalPoints = array_sum($categoryPoints);
+                            $totalPercentage = ($totalPoints / 1000) * 100;
+                        
+                            echo "<tr>
+                                    <td>Total</td>
+                                    <td>".array_sum($categoryEvents)."</td>
+                                    <td class='highlight_in_table'>$totalPoints / 1000</td>
+                                    <td class='highlight_in_table'>$totalPercentage%</td>
+                                </tr>";
+
+                        echo "</table>";
+
+                        $result->data_seek(0);
+
+                        echo "<h3>Events Attended by $username</h3>";
+                        echo "<table class='events-attended'>";
+                            echo "<tr>
+                                    <th>Sno</th>
+                                    <th>Event Name</th>
+                                    <th>Club Name</th>
+                                    <th>Category</th>
+                                    <th>Date</th>
+                                    <th>Time Slot</th>
+                                    <th>Points</th>
+                                </tr>";
+
+                                $sno = 1;
+
+                                while ($row = $result->fetch_assoc()) {
+                                echo "<tr>
+                                        <td>{$sno}</td>
+                                        <td>{$row['event_name']}</td>
+                                        <td>{$row['club_name']}</td>
+                                        <td>{$row['category']}</td>
+                                        <td>{$row['date_of_participation']}</td>
+                                        <td>{$row['time_slot']}</td>
+                                        <td>{$row['points']}</td>
+                                    </tr>";
+                                    $sno++;
+                                }
+
+                        echo "</table>";
+                    echo "</div>";
+                } else {
+                    echo "<p class='no_participation'>No participation records found for $username.<br><span>In case of any discrepancies, please fill the grievances form</span></p>";
+                }
+                $stmt->close();
+            }
+
+            $conn->close();
+            ?>
+        </div>
+
     </div>
 
-    <?php
-    require_once 'dbconn.php';
+    <footer>
+        <p>Designed and Developed by ZeroOne Code Club. All rights reserved</p>
+    </footer>
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-        $username = $_POST['username'];
-
-        $sql = "SELECT * FROM participants WHERE username = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param('s', $username);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            echo "<div class='participants_div'>";
-            
-            echo "<h3>Category-wise Points for $username</h3>";
-            echo "<table class='category_table'>";
-            echo "<tr>
-                    <th>Category</th>
-                    <th>Activities Participated</th>
-                    <th>Points</th>
-                    <th>Percentage</th>
-                  </tr>";
-
-            $categoryPoints = array('TEC' => 0, 'ESO' => 0, 'LCH' => 0, 'IIE' => 0, 'HWB' => 0);
-            $categoryEvents = array('TEC' => 0, 'ESO' => 0, 'LCH' => 0, 'IIE' => 0, 'HWB' => 0);
-
-            while ($row = $result->fetch_assoc()) {
-                // Update category points
-                $categoryPoints[$row['category']] += $row['points'];
-                // Update category events count
-                $categoryEvents[$row['category']] += 1;
-            }
-
-            foreach ($categoryPoints as $category => $points) {
-                echo "<tr>
-                        <td>$category</td>
-                        <td>{$categoryEvents[$category]}</td>
-                        <td>$points / 200</td>
-                        <td>".($points/2)."%</td>
-                      </tr>";
-            }
-
-            // Calculate and display the total points from all categories
-            $totalPoints = array_sum($categoryPoints);
-$totalPercentage = ($totalPoints / 1000) * 100; // Calculate the percentage out of 100
-echo "<tr>
-        <td>Total</td>
-        <td>".array_sum($categoryEvents)."</td>
-        <td class='highlight_in_table'>$totalPoints / 1000</td>
-        <td class='highlight_in_table'>$totalPercentage%</td>
-      </tr>";
-
-            echo "</table>";
-
-            // Reset the result pointer to the beginning
-            $result->data_seek(0);
-
-            // Display the table for events attended
-            echo "<h3>Events Attended by $username</h3>";
-            echo "<table class='events-attended'>";
-            echo "<tr>
-                    <th>Sno</th>
-                    <th>Event Name</th>
-                    <th>Club Name</th>
-                    <th>Category</th>
-                    <th>Venue</th>
-                    <th>Date</th>
-                    <th>Time Slot</th>
-                    <th>Points</th>
-                  </tr>";
-
-            $sno = 1;
-
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>
-                        <td>{$sno}</td>
-                        <td>{$row['event_name']}</td>
-                        <td>{$row['club_name']}</td>
-                        <td>{$row['category']}</td>
-                        <td>{$row['venue']}</td>
-                        <td>{$row['date_of_participation']}</td>
-                        <td>{$row['time_slot']}</td>
-                        <td>{$row['points']}</td>
-                      </tr>";
-                $sno++;
-            }
-
-            echo "</table>";
-            echo "</div>";
-        } else {
-            echo "<p class='no_participation'>No participation records found for $username.<br><span>In case of any discrepancies, please fill the grievances form</span></p>";
-
-        }
-
-        // Close the statement
-        $stmt->close();
-    }
-
-    // Close the database connection
-    $conn->close();
-    ?>
 </body>
- </html>
-
-
-
-
-
-
-
- 
+</html>
--- app/leaderboard.php
+++ app/leaderboard.php
@@ -4,39 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Leaderboard</title>
-    <link rel="stylesheet" href="./index.css">
-    <style>
-        body {
-            /* border: 0.1px solid transparent; */
-            height: 100vh;
-            margin: 0;
-            padding: 0;
-            display: flex;
-        }
-        .nav {
-            position: fixed;
-            width: 100%;
-        }
-        .participants_div {
-            margin-top: 5rem;
-        }
-        .participants_div table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        .participants_div table th {
-            background-color: black;
-            color: white;
-        }
-        .participants_div table th, .participants_div table td {
-            border: 1px solid grey;
-            padding: 5px;
-        }
-        
-    </style>
+    <link rel="stylesheet" href="./leaderboard.css">
 </head>
 <body>
-
 
     <div class="nav">
         <div class="nav-in">
@@ -46,52 +16,59 @@
             <div class="nav-two">
                 <a href="./index.php">Home</a>
                 <a href="./leaderboard.php">Leaderboard</a>
-                <!-- <a href="./view_activities.php">View Activities</a> -->
                 <a href="./add_grievance.php">Grievances</a>
                 <a href="./admin.php">Admin</a>
             </div>
         </div>
     </div>
 
-    <?php
-    require_once 'dbconn.php';
+    <div class="main-content">
+        <?php
+            require_once 'dbconn.php';
 
-    // Fetch student information from the participants table and calculate total points
-    $sql = "SELECT username, name, COUNT(*) AS activities_attended, SUM(points) AS total_points FROM participants GROUP BY username, name ORDER BY total_points DESC";
-    $result = $conn->query($sql);
+            $sql = "SELECT username, name, COUNT(*) AS activities_attended, SUM(points) AS total_points FROM participants GROUP BY username, name ORDER BY total_points DESC";
+            $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        // Display the leaderboard in a table
-        echo "<div class='participants_div'>";
-        echo "<table>";
-        echo "<tr>
-                <th>Rank</th>
-                <th>Username</th>
-                <th>Name</th>
-                <th>Activities Attended (SAC)</th>
-                <th>Total Points</th>
-              </tr>";
+            if ($result->num_rows > 0) {
+                echo "<div class='leaderboard_div'>";
+                echo "<table>";
+                echo "<tr>
+                        <th>Rank</th>
+                        <th>Username</th>
+                        <th>Name</th>
+                        <th>Activities Attended (SAC)</th>
+                        <th>Total Points</th>
+                    </tr>";
 
-        $rank = 1;
+                $rank = 1;
 
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>
-                    <td>{$rank}</td>
-                    <td>{$row['username']}</td>
-                    <td>{$row['name']}</td>
-                    <td>{$row['activities_attended']}</td>
-                    <td>{$row['total_points']}</td>
-                  </tr>";
-            $rank++;
-        }
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>
+                            <td>{$rank}</td>
+                            <td>{$row['username']}</td>
+                            <td>{$row['name']}</td>
+                            <td>{$row['activities_attended']}</td>
+                            <td>{$row['total_points']}</td>
+                        </tr>";
+                    $rank++;
+                }
 
-        echo "</table>";
-        echo "</div>";
-    } else {
-        echo "No data available in the leaderboard.";
-    }
+                echo "</table>";
+                echo "</div>";
+            } else {
+                echo "No data available in the leaderboard.";
+            }
 
-    $conn->close();
-    ?>
+            $conn->close();
+            ?>
+
+    </div>
+
+
+
+    <footer>
+        <p>Designed and Developed by ZeroOne Code Club. All rights reserved</p>
+    </footer>
+
 </body>
 </html>
